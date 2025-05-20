138dd88fe67ae1123f68da61277fef0c99e61438
Abiskar Lamichhane <abiskar.lamichhane@swifttech.com>
2025-05-20 12:09:59 +0545

    Testing cron event

---
+++ server/test/cryptojstest.js
@@ -0,0 +1,36 @@
+// const CryptoJS = require("crypto-js");
+
+// const testData = [
+//   {
+//     age: 22,
+//     name: "Abiskar",
+//   },
+//   {
+//     age: 22,
+//     name: "Abiskar"
+//   },
+// ];
+
+// const messageDigest = CryptoJS.HmacSHA1(
+//   JSON.stringify(testData[0]),
+//   "test123"
+// ).toString();
+
+// const msgd = CryptoJS.HmacSHA1(
+//   JSON.stringify(testData[1]),
+//   "test123"
+// ).toString();
+
+// if (messageDigest === msgd) {
+//   console.log("The digest are same ");
+// } else {
+//   console.log("Failed");
+// }
+
+// const secretKey = CryptoJS.enc.Utf8.parse("test123")
+
+
+// CryptoJS.AES.decrypt()
+
+// const sha1 = CryptoJS.SHA1("hello")
+// console.log(sha1)
